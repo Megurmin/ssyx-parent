281361db8e1ad241cbd1661cbd3fea544c77a147
HuangJin <tendenciesgo@gmail.com>
2024-02-08 01:32:43 +0800

    commit

--- service/service-payment/src/main/java/com/hj/ssyx/ServicePaymentApplication.java
+++ service/service-payment/src/main/java/com/hj/ssyx/ServicePaymentApplication.java
@@ -1,5 +1,6 @@
-package com.hj.ssyx.payment;
+package com.hj.ssyx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@MapperScan("com.hj.ssyx.payment.mapper")
 public class ServicePaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServicePaymentApplication.class,args);
